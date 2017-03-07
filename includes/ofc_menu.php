bb11c7dce4be7cd3de45eeec96d0bfb4d9d5e9d7
dhoegl <dhoegl@dhoegl6.redmond.corp.microsoft.com>
2017-03-06 22:15:28 -0700

    Initial Load

--- includes/ofc_menu.php
+++ includes/ofc_menu.php
@@ -7,7 +7,7 @@
 
 		}
 ?>
-<li><a href='/tecfamily.php'>Family Directory</a></li>
+<li><a href='/ofc_family.php'>Family Directory</a></li>
 
 <?php
 if($_SESSION['super_admin'] == 1 || $_SESSION['reg_admin'] == 1 || $_SESSION['pray_admin'] == 1 || $_SESSION['accesslevel'] == '3'){ // Administrator or Authorized user
@@ -28,7 +28,7 @@ if($_SESSION['super_admin'] == 1){ // SuperUser
 
 <?php
 if($_SESSION['accesslevel'] == '3'){ // Authorized User
- 	 echo "<li><a href='/tecfamview.php?id=" . $_SESSION['idDirectory'] . "'>My Profile</a></li>";
+ 	 echo "<li><a href='/ofc_famview.php?id=" . $_SESSION['idDirectory'] . "'>My Profile</a></li>";
 }
 ?>
 
