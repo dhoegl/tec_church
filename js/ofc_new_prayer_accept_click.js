f535861847024b5020a4aa9008252565f10bd91e
dhoegl <dhoegl@dhoegl6.redmond.corp.microsoft.com>
2017-03-09 13:26:34 -0700

    Initial Load

--- js/ofc_new_prayer_accept_click.js
+++ js/ofc_new_prayer_accept_click.js
@@ -6,10 +6,10 @@ var jQ150 = jQuery.noConflict();
 jQ150(document).ready(function () {
 //	jQ150("#acceptprayer #approvePrayer").on("click", '.prayer_approve', function () {
 	jQ150("#acceptprayer").click("#approvePrayer", function () {
-//		$approveclickbuttonid = prayerID;
+		$approveclickbuttonid = prayerID;
 		console.log("Approve Prayer button clicked");
 		console.log("$approve prayerid clicked = " + $approveclickbuttonid);
-//		$approveURL = "ofc_newprayeraccept.php?prayerid=" + $approveclickbuttonid;
+		$approveURL = "ofc_newprayeraccept.php?prayerid=" + $approveclickbuttonid;
 //		$approveURL = "bing.com";
 		console.log("approveURL = " + $approveURL);
 //		window.open($approveURL);
