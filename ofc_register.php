22fbbd8c985eebd28da47a06d2956244dde1fd0f
dhoegl <dhoegl@dhoegl6.redmond.corp.microsoft.com>
2018-01-19 23:48:27 -0800

    Initial Load

--- ofc_register.php
+++ ofc_register.php
@@ -1,5 +1,6 @@
 <?php 
 session_start();
+require_once 'ofc_dbconnect.php';
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -11,6 +12,8 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Please Register</title>
+<!-- Initialize jQuery scripts -->
+<script type="text/javascript" src="//code.jquery.com/jquery-latest.min.js"></script>
 
 
 <!-- Password Strength Check script -->
@@ -60,130 +63,126 @@ session_start();
             </div> <!-- Collapse Navbar -->
         </div> <!-- container -->
   </nav>
-<div class="container-fluid profile_bg">
+<div class="container">
     <div class="row">
-        <div class="col-md-4">
-            <div class="card bg-light border-primary text-center m-3">
-                <img class="card-img-top" src="images/trinity_logo_web.png" style="width: 75%; align-self: center" alt="Card image cap">
-                <div class="card-body">
-                    <h4 class="card-title">Please Register to access</h4>
-                    <p class="card-text">All fields must be filled in.</p>
-                    <p class="card-text"><strong>Note:</strong> Password must be at least 7 characters, contain one uppercase letter, contain one lowercase letter, and one number (0-9) or one special character.</p>
+        <div class="col-sm-6 bg-warning">
+            <h2 class="text-center">WELCOME</h2>
+                <h3 class="text-center">Please Register</h3>
+                <h4 align="center">All fields MUST be filled in</h4>
+                <h4 align="center"><strong>Note:</strong> Password must be at least 7 characters, contain one uppercase letter, one lowercase letter, and one number (0-9) or one special character.</h4>
+                <form name='form1' id="register" action='' method="POST">
+                    <table class="ofc_content" width="550" border="0" cellpadding="1" cellspacing="1" >
+                        <tr>
+                            <td>
+                                <p></p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p></p>
+                            </td>
+                        </tr>
+                        <tr>
+                                <td align="right">
+                                Select a User Name:
+                                </td>
+                                <td>
+                                <input type='text' name='username' value="<?php echo $username; ?>">
+                                </td>
+                        </tr>
+                        <tr>
+                                <td align="right">
+                                Choose a Password:
+                                </td>
+                                <td>
+                                <input name="password" id="password" type="password"/>
+                                <span id="register_result"></span>
+                                </td>
+                        </tr>
+                        <tr>
+                                <td align="right">
+                                Re-enter your Password:
+                                </td>
+                                <td>
+                                <input type='password' name='repeatpassword'>
+                                </td>
+                        </tr>
+                                    <tr>
+                                <td align="right">
+                                Your First Name:
+                                </td>
+                                <td>
+                                <input type='text' name='firstname' value="<?php echo $firstname; ?>">
+                                </td>
+                        </tr>
+                        <tr>
+                                <td align="right">
+                                Your Last Name:
+                                </td>
+                                <td>
+                                <input type='text' name='lastname' value="<?php echo $lastname; ?>">
+                                </td>
+                        </tr>
+                        <tr>
+                                <td align="right">Gender:</td>
+                                <td><input type='radio' name='gender' value="M"> M</td>
+                        </tr>
+                        <tr>
+                                <td width="50%">&nbsp</td>
+                                <td ><input type='radio' name='gender' value="F"> F</td>
+                        </tr>
+                        <tr>
+                                <td align="right">
+                                Your email address:
+                                </td>
+                                <td>
+                                <input type='email' name='emailaddr' value="<?php echo $emailaddr; ?>">
+                                </td>
+                        </tr>
+                        <tr>
+                                <td align="right">
+                                Re-enter your email address:
+                                </td>
+                                <td>
+                                <input type='email' name='repeatemailaddr' value="<?php echo $repeatemailaddr; ?>">
+                                </td>
+                        </tr>
+                        <tr>
+                            <td>
+                            </td>
+                            <td>
+                            <table>
+                            <tr>
+                                    <td>
+                                            <input type='submit' id="register_submit" name='submit' value='Register'>
+                                    </td>
+                                    <td>
+                                            <input type='submit' name='clear' value='Clear'>
+                                    </td>
+                            </tr>
+                            </table>
+                            </td>
+                        </tr>
+                 </table>
+                    <p>
+                    <br />
+                </form>
+            </div> <!--col-sm-6-->
+<!--        </div> row
 
-                    <form name='form1' id="register" action='' method="POST">
-                            <table class="ofc_content" width="550" border="0" cellpadding="1" cellspacing="1" >
-                                    <tr>
-                                        <td>
-                                            <p></p>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p></p>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                            <td align="right">
-                                            Select a User Name:
-                                            </td>
-                                            <td>
-                                            <input type='text' name='username' value="<?php echo $username; ?>">
-                                            </td>
-                                    </tr>
-                                    <tr>
-                                            <td align="right">
-                                            Choose a Password:
-                                            </td>
-                                            <td>
-                                            <input name="password" id="password" type="password"/>
-                                            <span id="register_result"></span>
-                                            </td>
-                                    </tr>
-                                    <tr>
-                                            <td align="right">
-                                            Re-enter your Password:
-                                            </td>
-                                            <td>
-                                            <input type='password' name='repeatpassword'>
-                                            </td>
-                                    </tr>
-<!--                                    <tr>
-                                            <td align="right">
-                                            Your First Name:
-                                            </td>
-                                            <td>
-                                            <input type='text' name='firstname' value="<?php echo $firstname; ?>">
-                                            </td>
-                                    </tr>
-                                    <tr>
-                                            <td align="right">
-                                            Your Last Name:
-                                            </td>
-                                            <td>
-                                            <input type='text' name='lastname' value="<?php echo $lastname; ?>">
-                                            </td>
-                                    </tr>
-                                    <tr>
-                                            <td align="right">Gender:</td>
-                                            <td><input type='radio' name='gender' value="M">M</td>
-                                    </tr>
-                                    <tr>
-                                            <td width="50%">&nbsp</td>
-                                            <td ><input type='radio' name='gender' value="F">F</td>
-                                    </tr>
-                                    <tr>
-                                            <td align="right">
-                                            Your email address:
-                                            </td>
-                                            <td>
-                                            <input type='text' name='emailaddr' value="<?php echo $emailaddr; ?>">
-                                            </td>
-                                    </tr>
-                                    <tr>
-                                            <td align="right">
-                                            Re-enter your email address:
-                                            </td>
-                                            <td>
-                                            <input type='text' name='repeatemailaddr' value="<?php echo $repeatemailaddr; ?>">
-                                            </td>
-                                    </tr>
-                                    <tr>
-                                            <td>
-                                            </td>
-                                            <td>
-                                                    <table>
-                                                    <tr>
-                                                            <td>
-                                                                    <input type='submit' id="register_submit" name='submit' value='Register'>
-                                                            </td>
-                                                            <td>
-                                                                    <input type='submit' name='clear' value='Clear'>
-                                                            </td>
-                                                    </tr>
-                                                    </table>
-                                            </td>
-                                    </tr>
--->                            </table>
-                            <p>
-                            <br />
-                    </form>
-                </div> <!-- card-body -->
-            </div> <!-- card -->
-
-        </div> <!-- col-md-4 -->
-        
-        <div class="col-md-4">
-            <div class="card bg-light border-primary text-center m-3">
-                <img class="card-img-top" src="images/trinity_logo_web.png" style="width: 75%; align-self: center" alt="Card image cap">
-                <div class="card-body">
-                    <h4 class="card-title">What happens next</h4>
-                    <p>After completing the entry form at the left, our administrators will verify and approve your request to access our site.</p>
-                    <p>You will be notified via email (using the address at left) that your access has been granted.</p>
-                    <p>If you don't receive an email notification within 48 hours <strong>(don't forget to check your Junk Mail folder)</strong>, please contact one of our church elders for assistance.</p>
-		</div> <!-- card-body -->
-            </div> <!-- card -->
-        </div> <!-- col-md-4 -->
-    </div> <!-- row -->
+        <div class="row"> -->
+            <div class="col-sm-6">
+                <div class="card bg-light border-primary text-center m-3">
+                    <img class="card-img-top" src="images/trinity_logo_web.png" style="width: 75%; align-self: center" alt="Card image cap">
+                    <div class="card-body">
+                        <h3 class="card-title">What happens next</h3>
+                        <p>After completing the entry form at the left, our administrators will verify and approve your request to access our site.</p>
+                        <p>You will be notified via email (using the address at left) that your access has been granted.</p>
+                        <p>If you don't receive an email notification within 48 hours <strong>(don't forget to check your Junk Mail folder)</strong>, please contact one of our church elders for assistance.</p>
+                    </div> <!-- card-body -->
+                </div> <!-- card -->
+            </div> <!-- col-sm-6 -->
+        </div> <!-- row -->
 </div> <!-- container -->
 
 <?php
@@ -235,7 +234,7 @@ if($clear)
 						$regentryquery = "SELECT username from " . $_SESSION['logintablename'] . " WHERE username = '" . $username . "'";
 						$regentryexist = $mysql->query($regentryquery) or die("A database error has occurred. Please notify your administrator with the following. Error : " . mysql_errno() . mysql_error());
 
-						$regentrynumrows = $regentryexist->num_rows();
+						$regentrynumrows = $regentryexist->num_rows;
 						if($regentrynumrows == 0)
 						{
 							if($gender == 'M') 
@@ -246,7 +245,7 @@ if($clear)
 							{
 								$regquery = $mysql->query("INSERT INTO " . $_SESSION['dirtablename'] . " (Name_2, Surname, Email_2, Picture_Link, Picture_Taken) VALUES ('$firstname','$lastname','$emailaddr', 'x', 'N')") or die("Unable to create new user record in directory. Error : " . mysql_errno() . mysql_error());
 							}
-							$regInsertID = $mysql->insert_id();
+							$regInsertID = $mysql->insert_id;
 							$reglogliquery = $mysql->query("INSERT INTO " . $_SESSION['logintablename'] . " (username, password, idDirectory, firstname, lastname, gender, email_addr) VALUES ('$username','$password','$regInsertID','$firstname','$lastname','$gender','$emailaddr')") or die("Unable to create new login record in directory. Error : " . mysql_errno() . mysql_error());
 							if(!$regquery)
 								{
@@ -269,14 +268,14 @@ if($clear)
 										$regmailtest = $regmailrow['email_addr'];									
 										$regmailto = $regmailtest . " , " . $regmailto;
 									}									
-									$regmailsubject = "Membership Request to Our Family Connections"."\n..";
-									$regmailmessage = "Hello! " . "\n" . $firstname . " " . $lastname . " has requested to be added to Our Family Connections.\n\nLogin to our site using your admin credentials, select the <strong>Access Admin</strong> menu item, and accept or reject this request \n\n" . $regmaillink;
+									$regmailsubject = "Membership Request to (OFC_TEST) Our Family Connections"."\n..";
+									$regmailmessage = "Hello (OFC_TEST)! " . "\n" . $firstname . " " . $lastname . " has requested to be added to Our Family Connections.\n\nLogin to our site using your admin credentials, select the <strong>Access Admin</strong> menu item, and accept or reject this request \n\n" . $regmaillink;
 									$regmailfrom = "newfamilyrequest@ourfamilyconnections.org";
 									$regmailheaders = "From:" . $regmailfrom . "\r\n";
 									$regmailheaders .= "MIME-Version: 1.0\r\n";
 									$regmailheaders .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 									mail($regmailto,$regmailsubject,$regmailmessage,$regmailheaders);
-									echo "<center><table id='profiletable'><tr><td class='strong'>Hi " . $firstname . "!  <br><br>You should receive a welcome email within 48 hours<br>Thank you for your patience!<br><br><a href='tecwelcome.php'>Return to Login Page</a></td></tr></table></center>";
+									echo "<center><table id='profiletable'><tr><td class='strong'>Hi " . $firstname . "!  <br><br>(OFC_TEST) You should receive a welcome email within 48 hours<br>Thank you for your patience!<br><br><a href='tecwelcome.php'>Return to Login Page</a></td></tr></table></center>";
 								}
 							
 						}
@@ -303,8 +302,8 @@ if($clear)
 		}
 	}
 ?>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	
+-->	
 </body>
 </html>
